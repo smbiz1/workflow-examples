3dc652be53e5efa07ae12e0ec9ea850356e5dc31
Pranay Prakash <pranay.gp@gmail.com>
2026-01-08 00:38:56 -0800

    Add instrumentation.ts to nextjs example

--- nextjs/app/api/signup/route.ts
+++ nextjs/app/api/signup/route.ts
@@ -3,12 +3,14 @@ import { handleUserSignup } from "@/workflows/user-signup";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
-	const { email } = await request.json();
+  const { email } = await request.json();
 
-	// Executes asynchronously and doesn't block your app
-	await start(handleUserSignup, [email]);
+  // Executes asynchronously and doesn't block your app
+  const run = await start(handleUserSignup, [email]);
 
-	return NextResponse.json({
-		message: "User signup workflow started",
-	});
+  console.log("Started run", run.runId);
+
+  return NextResponse.json({
+    message: "User signup workflow started",
+  });
 }
---
+++ nextjs/instrumentation.ts
@@ -0,0 +1,7 @@
+// Optional: only REQUIRED when using a world with asynchronous workers, like Postgres World
+if (process.env.NEXT_RUNTIME !== "edge") {
+  import("workflow/runtime").then(async ({ getWorld }) => {
+    console.log("Calling world.start()");
+    await getWorld().start?.();
+  });
+}
